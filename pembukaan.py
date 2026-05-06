11b315493ca851a870b59392ae69b4d179c3f9a7
phunk-pentium-one <diditseptian220@gmail.com>
2026-05-06 17:19:19 +0700

    Add files via upload

---
+++ pembukaan.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# 1. Membuat jendela utama
+root = tk.Tk()
+
+# 2. Memberi judul di pojok kiri atas
+root.title("Kasir Dasar")
+
+# 3. Mengatur ukuran jendela (lebar x tinggi)
+root.geometry("400x400")
+
+# 4. Menjalankan jendela agar tetap muncul
+root.mainloop()
